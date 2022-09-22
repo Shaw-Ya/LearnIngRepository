c49e6e1a394ba225e1a0fa8521150b0fe33e7c96
Ya Zhiheng <shaw_ya@qq.com>
2022-09-22 10:41:20 +0800

    init

---
+++ src/HelloWorld/demo/calculate.java
@@ -0,0 +1,56 @@
+package HelloWorld.demo;
+
+public class calculate {
+
+
+    /* float a, b, c;
+    public void setnum(float A, float B){
+    a = A;
+    b = B;
+    }
+    public float math(){
+        c = a / b;
+        return  c;
+    }
+    public void main(String arg[]){
+        calculate num = new calculate();
+        num.setnum(5,2);
+
+        System.out.println(num.math());
+    }*/
+
+
+
+
+   /*  float a = 3.3f;
+    float b = 4.8f;
+    float c = a + b;
+    public  void main(String arg[]){
+
+        System.out.println(c);
+    }*/
+
+
+
+        /*public static void main(String args[]){
+            Integer x =Integer.valueOf(9);
+            Double c = Double.valueOf(5);
+            Float a = Float.valueOf("80");
+
+            Integer b = Integer.valueOf("444",16);   // 使用 16 进制
+
+            System.out.println(x);
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println(b);
+        }*/
+
+
+                public static void main(String args[]) {
+                        Integer[] data = new Integer[1];
+                        System.out.println(data[0]);
+                }
+
+
+
+}
